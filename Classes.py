fd9cfbebe776533a494e82a110469b1dc2d1e424
Sergey <tsv19su@yandex.ru>
2024-04-03 20:38:15 +0300

    Правки 3 апреля 2024 года

--- Classes.py
+++ Classes.py
@@ -813,8 +813,6 @@ class Servers:
                 print(" template = " + str(xml_to_String))
                 #print(" dump = " + str(ElementTree.dump(root_tag)))
                 print(" root tag name = " + str(root_tag.tag))
-                XMLQuery = "UPDATE dbo.AirPortsTable SET LogDateAndTimeViewed = '" + str(xml_to_String) + "' "
-                self.seekRT.execute(XMLQuery)
             else:
                 tree_from_XML_as_a_SAX_using_xml = ElementTree.parse(ResultXML[0])  # указатель на XML-ную структуру
                 root_tag = tree_from_XML_as_a_SAX_using_xml.getroot()  # становимся на корневой тэг
@@ -823,17 +821,27 @@ class Servers:
                 root_tag_Name = root_tag.tag  # имя корневого тэга
                 root_tag_Attr = root_tag.attrib  # аттрибут(ы) корневого тэга в виде словаря
             SQLQuery = "UPDATE dbo.AirPortsTable SET LogCountViewed = " + str(Count)
+            XMLQuery = "UPDATE dbo.AirPortsTable SET LogDateAndTimeViewed = '" + str(xml_to_String) + "' "
             if iata is None:
-                SQLQuery += " WHERE AirPortCodeIATA IS NULL AND AirPortCodeICAO = '" + str(icao) + "' "
+                Append = " WHERE AirPortCodeIATA IS NULL AND AirPortCodeICAO = '" + str(icao) + "' "
+                SQLQuery += Append
+                XMLQuery += Append
             elif icao is None:
-                SQLQuery += " WHERE AirPortCodeIATA = '" + str(iata) + "' AND AirPortCodeICAO IS NULL "
+                Append = " WHERE AirPortCodeIATA = '" + str(iata) + "' AND AirPortCodeICAO IS NULL "
+                SQLQuery += Append
+                XMLQuery += Append
             elif iata is None and icao is None:
-                SQLQuery += " WHERE AirPortCodeIATA IS NULL AND AirPortCodeICAO IS NULL "
+                Append = " WHERE AirPortCodeIATA IS NULL AND AirPortCodeICAO IS NULL "
+                SQLQuery += Append
+                XMLQuery += Append
             else:
-                SQLQuery += " WHERE AirPortCodeIATA = '" + str(iata) + "' AND AirPortCodeICAO = '" + str(icao) + "' "
+                Append = " WHERE AirPortCodeIATA = '" + str(iata) + "' AND AirPortCodeICAO = '" + str(icao) + "' "
+            SQLQuery += Append
+            XMLQuery += Append
             self.seekRT.execute(SQLQuery)
-            ResultSQL = True
+            self.seekRT.execute(XMLQuery)
             self.cnxnRT.commit()
+            ResultSQL = True
         except Exception:
             ResultSQL = False
             self.cnxnRT.rollback()

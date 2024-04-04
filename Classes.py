95567e1eb2911aa0e1a5b32500ab92626a825f54
Sergey <tsv19su@yandex.ru>
2024-04-04 12:58:42 +0300

    Правки 3 апреля 2024 года

--- Classes.py
+++ Classes.py
@@ -821,11 +821,11 @@ class Servers:
                 Search = root_tag.find('.//User')  # список указателей
                 print(" Search = " + str(Search))
                 if Search.attrib['Name'] == user:
-                    print("Добавляем новую подветку с отметкой времени и с Host-ом")
+                    print("Добавляем еще одну подветку с отметкой времени и с Host-ом")
                     newDateTime = ElementTree.SubElement(User, 'DateTime')  # fixme который User?
-                    newDateTime.attrib['From'] = str(host)  # fixme новая подветка не дописалась
+                    newDateTime.attrib['From'] = str(host)  # fixme еще одна подветка не дописалась
                     #root_tag.insert(3, DateTime)  # вставилась 3-я по счету подветка (не по схеме)
-                    #User.append(DateTime)  # fixme новая подветка не дописалась
+                    #User.append(DateTime)  # fixme еще одна подветка не дописалась
                 else:
                     print("Добавляем новую подветку с новым User-ом и с отметкой времени с Host-ом")
                     User.append(DateTime)

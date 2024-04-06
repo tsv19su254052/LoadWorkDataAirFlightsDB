89bdb75293ffc198824e582fea8926e1eadfc3b7
Sergey <tsv19su@yandex.ru>
2024-04-06 10:31:58 +0300

    Правки 6 апреля 2024 года

--- Classes.py
+++ Classes.py
@@ -805,7 +805,7 @@ class Servers:
                 Count += 1
             print("LogCountViewed = " + str(Count))
             host = 'WorkCompTest1'
-            user = 'ArtemTest13'
+            user = 'ArtemTest14'
             print(" ResultXML = " + str(ResultXML[0]))
             DateTime = ElementTree.Element('DateTime', From=str(host))
             DateTime.text = str(dtn)

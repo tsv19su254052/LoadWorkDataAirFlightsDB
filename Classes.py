2fbbac743a50bae02f5ff5de93b60147aebdb4ef
Sergey <tsv19su@yandex.ru>
2024-04-29 14:32:06 +0300

    Правки 02 мая 2024 года

--- Classes.py
+++ Classes.py
@@ -981,9 +981,36 @@ class Servers:
                     try:
                         SQLQuery = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"
                         self.seekAC_XML.execute(SQLQuery)
-                        XMLQuery = "SELECT * FROM dbo.AirCraftsTableNew2XsdIntermediate WITH (UPDLOCK) WHERE AirCraftRegistration = '" + str(ac) + "' "
+                        XMLQuery = "SELECT FlightsByRoutes, RoutesByFlights FROM dbo.AirCraftsTableNew2XsdIntermediate WITH (UPDLOCK) WHERE AirCraftRegistration = '" + str(ac) + "' "
                         self.seekAC_XML.execute(XMLQuery)
                         ResultXML = self.seekAC_XML.fetchone()
+                        if ResultXML[0] is None:
+                            root_tag_FlightsByRoutes = ElementTree.Element('FlightsByRoutes')
+                        else:
+                            root_tag_FlightsByRoutes = ElementTree.fromstring(ResultXML[0])
+                            Search = root_tag_FlightsByRoutes.findall(".//Route")
+                            added = False
+                            for node in Search:
+                                if node.attrib['RouteFK=' + str(db_air_route)]:
+                                    node.append()
+                                    added = True
+                            if not added:
+                                pass
+                        xml_FlightsByRoutes_to_String = ElementTree.tostring(root_tag_FlightsByRoutes, method='xml').decode(encoding="utf-8")  # XML-ная строка
+                        if ResultXML[1] is None:
+                            root_tag_RoutesByFlights = ElementTree.Element('RoutesByFlights')
+                        else:
+                            root_tag_RoutesByFlights = ElementTree.fromstring(ResultXML[1])
+                            Search = root_tag_RoutesByFlights.findall(".//Flight")
+                            added = False
+                            for node in Search:
+                                if node.attrib['FlightNumberString=' + str(al) + str(fn)]:
+                                    node.append()
+                                    added = True
+                            if not added:
+                                pass
+                        xml_RoutesByFlights_to_String = ElementTree.tostring(root_tag_RoutesByFlights, method='xml').decode(encoding="utf-8")  # XML-ная строка
+                        XMLQuery = "UPDATE dbo.AirCraftsTableNew2XsdIntermediate SET FlightsByRoutes = '" + str(xml_FlightsByRoutes_to_String) + "', RoutesByFlights = '" + str(xml_RoutesByFlights_to_String) + "' WHERE AirCraftRegistration = '" + str(ac) + "' "
                     except Exception:
                         ResultSQL = 0  # не сработка
                         self.cnxnFN.rollback()
